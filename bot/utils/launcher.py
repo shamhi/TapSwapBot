51dea380badfe043c11c3a6f1a66d3c44d3e4996
shamhi <shomakakdela@gmail.com>
2024-04-07 11:57:11 +0300

    ✨ Add launch via Telegram

--- bot/utils/launcher.py
+++ bot/utils/launcher.py
@@ -4,7 +4,7 @@ import asyncio
 import argparse
 from itertools import cycle
 
-from pyrogram import Client
+from pyrogram import Client, compose
 from better_proxy import Proxy
 
 from bot.config import settings
@@ -23,19 +23,23 @@ Select an action:
 
     1. Create session
     2. Run clicker
+    3. Run via Telegram (Beta)
 """
 
+global tg_clients
 
 def get_session_names() -> list[str]:
-    session_names = glob.glob('sessions/*.session')
-    session_names = [os.path.splitext(os.path.basename(file))[0] for file in session_names]
+    session_names = glob.glob("sessions/*.session")
+    session_names = [
+        os.path.splitext(os.path.basename(file))[0] for file in session_names
+    ]
 
     return session_names
 
 
 def get_proxies() -> list[Proxy]:
     if settings.USE_PROXY_FROM_FILE:
-        with open(file='bot/config/proxies.txt', encoding='utf-8-sig') as file:
+        with open(file="bot/config/proxies.txt", encoding="utf-8-sig") as file:
             proxies = [Proxy.from_str(proxy=row.strip()).as_url for row in file]
     else:
         proxies = []
@@ -44,6 +48,8 @@ def get_proxies() -> list[Proxy]:
 
 
 async def get_tg_clients() -> list[Client]:
+    global tg_clients
+
     session_names = get_session_names()
 
     if not session_names:
@@ -52,20 +58,23 @@ async def get_tg_clients() -> list[Client]:
     if not settings.API_ID or not settings.API_HASH:
         raise ValueError("API_ID and API_HASH not found in the .env file.")
 
-    tg_clients = [Client(
-        name=session_name,
-        api_id=settings.API_ID,
-        api_hash=settings.API_HASH,
-        workdir='sessions/',
-        plugins=dict(root='bot/plugins')
-    ) for session_name in session_names]
+    tg_clients = [
+        Client(
+            name=session_name,
+            api_id=settings.API_ID,
+            api_hash=settings.API_HASH,
+            workdir="sessions/",
+            plugins=dict(root="bot/plugins"),
+        )
+        for session_name in session_names
+    ]
 
     return tg_clients
 
 
 async def process() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--action', type=int, help='Action to perform')
+    parser.add_argument("-a", "--action", type=int, help="Action to perform")
 
     logger.info(f"Detected {len(get_session_names())} sessions | {len(get_proxies())} proxies")
 
@@ -79,8 +88,8 @@ async def process() -> None:
 
             if not action.isdigit():
                 logger.warning("Action must be number")
-            elif action not in ['1', '2']:
-                logger.warning("Action must be 1 or 2")
+            elif action not in ["1", "2", "3"]:
+                logger.warning("Action must be 1, 2 or 3")
             else:
                 action = int(action)
                 break
@@ -91,12 +100,25 @@ async def process() -> None:
         tg_clients = await get_tg_clients()
 
         await run_tasks(tg_clients=tg_clients)
+    elif action == 3:
+        tg_clients = await get_tg_clients()
+
+        logger.info("Send /help command in Saved Messages\n")
+
+        await compose(tg_clients)
 
 
 async def run_tasks(tg_clients: list[Client]):
     proxies = get_proxies()
     proxies_cycle = cycle(proxies) if proxies else None
-    tasks = [asyncio.create_task(run_tapper(tg_client=tg_client, proxy=next(proxies_cycle) if proxies_cycle else None))
-             for tg_client in tg_clients]
+    tasks = [
+        asyncio.create_task(
+            run_tapper(
+                tg_client=tg_client,
+                proxy=next(proxies_cycle) if proxies_cycle else None,
+            )
+        )
+        for tg_client in tg_clients
+    ]
 
     await asyncio.gather(*tasks)
