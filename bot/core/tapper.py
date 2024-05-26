f2b519c8e590953a4fdfba017f3975a7adfda216
shamhi <shomakakdela@gmail.com>
2024-05-26 19:18:27 +0300

    📌 Initial commit

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -164,6 +164,7 @@ class Tapper:
         access_token_created_time = 0
         turbo_time = 0
         active_turbo = False
+        errors_count = 0
 
         proxy_conn = ProxyConnector().from_url(proxy) if proxy else None
 
@@ -178,6 +179,10 @@ class Tapper:
 
             while True:
                 try:
+                    if errors_count > 10:
+                        logger.info(f"{self.session_name} | Errors count: <r>{errors_count}</r> | Next session pack")
+                        return
+
                     if time() - access_token_created_time >= 1800:
                         tg_web_data = await self.get_tg_web_data(proxy=proxy)
                         profile_data, access_token = await self.login(http_client=http_client, tg_web_data=tg_web_data)
@@ -230,6 +235,7 @@ class Tapper:
                             status="ERROR",
                             amount=balance,
                         )
+                        errors_count += 1
                         continue
 
                     available_energy = player_data['energy']
