47c603722d547d4c995ef14a9c7d0b09244a3d66
shamhi <shomakakdela@gmail.com>
2024-06-26 23:18:48 +0300

    🖌 Cleanup code

--- bot/utils/launcher.py
+++ bot/utils/launcher.py
@@ -1,24 +1,24 @@
-import os
-import glob
 import asyncio
 import argparse
 from itertools import cycle
 
 from pyrogram import Client, compose
-from better_proxy import Proxy
 
 from bot.config import settings
 from bot.utils import logger
 from bot.core.tapper import run_tapper
 from bot.core.registrator import register_sessions
+from bot.utils.scripts import get_session_names, get_proxies
 
-
-start_text = """
+banner = """
 
 ▀▀█▀▀ █▀▀█ █▀▀█ ░█▀▀▀█ █   █ █▀▀█ █▀▀█ ░█▀▀█ █▀▀█ ▀▀█▀▀
  ░█   █▄▄█ █  █  ▀▀▀▄▄ █▄█▄█ █▄▄█ █  █ ░█▀▀▄ █  █   █
  ░█   ▀  ▀ █▀▀▀ ░█▄▄▄█  ▀ ▀  ▀  ▀ █▀▀▀ ░█▄▄█ ▀▀▀▀   ▀
 
+"""
+
+options = """
 Select an action:
 
     1. Create session
@@ -26,25 +26,8 @@ Select an action:
     3. Run via Telegram (Beta)
 """
 
+
 global tg_clients
-
-def get_session_names() -> list[str]:
-    session_names = glob.glob("sessions/*.session")
-    session_names = [
-        os.path.splitext(os.path.basename(file))[0] for file in session_names
-    ]
-
-    return session_names
-
-
-def get_proxies() -> list[Proxy]:
-    if settings.USE_PROXY_FROM_FILE:
-        with open(file="bot/config/proxies.txt", encoding="utf-8-sig") as file:
-            proxies = [Proxy.from_str(proxy=row.strip()).as_url for row in file]
-    else:
-        proxies = []
-
-    return proxies
 
 
 async def get_tg_clients() -> list[Client]:
@@ -76,12 +59,14 @@ async def process() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--action", type=int, help="Action to perform")
 
+    print(banner)
+
     logger.info(f"Detected {len(get_session_names())} sessions | {len(get_proxies())} proxies")
 
     action = parser.parse_args().action
 
     if not action:
-        print(start_text)
+        print(options)
 
         while True:
             action = input("> ")
